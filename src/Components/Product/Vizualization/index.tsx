f71962bcc23c21b4c612e0d7d58d09dd7f12212e
regis <gbrregispf@gmail.com>
2022-09-04 18:03:08 -0300

    feat: Vizualization

--- src/Components/Product/Vizualization/index.tsx
+++ src/Components/Product/Vizualization/index.tsx
@@ -4,11 +4,10 @@ import { ImageContainer } from "./styles";
 const ProductPreview = () =>{
   return (
   <ImageContainer>
-   <img src="https://www.apple.com/newsroom/images/product/mac/standard/Apple_MacBook-Pro_14-16-inch_10182021_big.jpg.large.jpg" 
+    <img src="https://www.apple.com/newsroom/images/product/mac/standard/Apple_MacBook-Pro_14-16-inch_10182021_big.jpg.large.jpg" 
    alt="Macbook" 
     />
-   <span>Roll over image to zoom in</span>
-
+    <span>Roll over image to zoom in</span>
   </ImageContainer>
   )
 }
